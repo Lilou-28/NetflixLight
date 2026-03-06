a12e5c48bc4b3f7ff486350c0a1e11467bf6cb52
rlilou <lilou.rouquette@ynov.com>
2026-03-06 14:43:37 +0100

    feat(all): change in server operation

--- cmd/server.js
+++ cmd/server.js
@@ -1,21 +1,29 @@
 const http = require('http')
-const fs = require('fs').promises
+const fs = require('fs')
+const path = require("path");
 
 const host = 'localhost'
 const port = 8080
 
 const server = http.createServer((req, res) => {
-    fs.readFile(__dirname + "/../web/templates/index.html")
-    .then(contents => {
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'text/html')
-        res.end(contents)
-    })
-    .catch(err => {
-        res.writeHead(500)
-        res.end(String(err))
-        return
-    })
+    if (req.url === "/") {
+        fs.readFile(path.join(__dirname,"../web/templates/index.html"), (err, data) => {
+            res.writeHead(200, {"Content-Type" : "text/html" })
+            res.end(data)
+        })
+    }
+
+    else if (req.url === "/details"){
+        fs.readFile(path.join(__dirname, "../web/templates/detail.html"), (err,data) => {
+            res.writeHead(200, {"Content-Type" :  "text/html"})
+            res.end(data)
+        })
+    }
+    
+    else {
+        res.writeHead(404, {"Content-Type" : "text/plain"})
+        res.end("Page non trouvée")
+    }
 })
 
 server.listen(port, host, () => {
