ab28d84edfe5f93fbb6b22a40e508c904839952f
rlilou <lilou.rouquette@ynov.com>
2026-03-06 16:15:25 +0100

    feat(all): add db and base of register and login

--- cmd/server.js
+++ cmd/server.js
@@ -1,9 +1,21 @@
 const http = require('http')
 const fs = require('fs')
 const path = require("path");
+const db = require("../internal/database")
 
 const host = 'localhost'
 const port = 8080
+
+const mimeTypes = {
+    ".css": "text/css",
+    ".js": "application/javascript",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
+    ".svg": "image/svg+xml",
+    ".ico": "image/x-icon",
+    ".html": "text/html",
+};
 
 const server = http.createServer((req, res) => {
     if (req.url === "/") {
@@ -16,6 +28,22 @@ const server = http.createServer((req, res) => {
     else if (req.url === "/details"){
         fs.readFile(path.join(__dirname, "../web/templates/detail.html"), (err,data) => {
             res.writeHead(200, {"Content-Type" :  "text/html"})
+            res.end(data)
+        })
+    }
+
+    else if (req.url && req.url.startsWith("/static/")) {
+        const filePath = path.join(__dirname, "../web", req.url)
+        const ext = path.extname(filePath).toLowerCase()
+        const contentType = mimeTypes[ext] || "application/octet-stream"
+
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(404, {"Content-Type" : "text/plain"})
+                res.end("Fichier statique introuvable")
+                return
+            }
+            res.writeHead(200, {"Content-Type" : contentType})
             res.end(data)
         })
     }
