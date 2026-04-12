2f7dd95acf3a29cecca70dc832c952459a33bc17
Marie-Catalina Grenot <mariecatalina.grenot@ynov.com>
2026-04-12 19:16:17 +0200

    feat(server): add series route

--- cmd/server.js
+++ cmd/server.js
@@ -567,6 +567,25 @@ const server = http.createServer((req, res) => {
             res.end(data)
         })
     }
+    else if (req.method === "GET" && req.url === "/series") {
+    const sessionToken = getSessionTokenFromCookie(req);
+    if (!sessionToken) {
+        res.writeHead(302, { "Location": "/login" });
+        res.end();
+        return;
+    }
+    checkToken(sessionToken, (isValid) => {
+        if (!isValid) {
+            res.writeHead(302, { "Location": "/login" });
+            res.end();
+            return;
+        }
+        fs.readFile(path.join(__dirname, "../web/templates/series.html"), (err, data) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+        });
+    });
+}
     else if (req.url.startsWith("/search-movie") && req.method === "GET") {
 
         const urlObj = new URL(req.url, `http://${req.headers.host}`);
